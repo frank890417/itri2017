6324b1a3ff83f26430c6efb949c00271c12e9a0b
cheyuwu <frank890417@gmail.com>
2017-09-22 18:35:41 +0800

    edit room devices to multiple acceptable

--- resources/assets/js/components/page_room.vue
+++ resources/assets/js/components/page_room.vue
@@ -14,7 +14,7 @@
           //房間選擇
           .btn_group.btn_room_selector(:class="{fixed: site_width<600 && inPageRange}")
             h1(v-for="(room,rid) in rooms",:class="{active:rid==now_place_id}",@click="switch_place(rid)") {{room.name}}
-               
+            h1.manage +管理房間 
           h4 {{rooms[now_place_id].slogan}}
 
           //電表跟數字
@@ -58,45 +58,54 @@
                   span {{device.name}}
                   // div (數量:{{device.count}} {{["很少","偶爾","經常","頻繁"][device.option]}}使用 {{ ["0-3年","3-5年","5-10年","10年以上"][device.buy_time] }})
 
+          .form_block.spec_select
+            ul 
+              li(:class="{active: alter_id==-1}"
+                 @click="alter_id=-1") {{now_device.consumption}}w x {{now_device.count}}
+              li(:class="{active: alter_id==alt_id}",
+                v-if="now_device.alter_specs" 
+                v-for="(alt,alt_id) in now_device.alter_specs",
+                @click="alter_id=alt_id") {{alt.consumption}}w x {{alt.count}}
+              li.more(@click="add_other_spec(now_device)") +
           //電器消耗
           .form_block
             .form-group(v-if="now_device.type=='normal'")
               label 電器瓦數
-              input(type="number",v-model="now_device.consumption")
+              input(type="number",v-model="now_device_profile.consumption")
             .form-group(v-if="now_device.type=='light'")
               ul.room_device_list
-                li(:class="{active:now_device.light_option==lid}",
-                   @click="now_device.light_option=lid",
+                li(:class="{active:now_device_profile.light_option==lid}",
+                   @click="now_device_profile.light_option=lid",
                    v-for="(light,lid) in light_list") {{light.name}}
             .form-group
               label 數量
-              span.input_side_btn(@click="now_device.count-=now_device.count>0?1:0") -
-              input(type="number" ,v-model="now_device.count")
-              span.input_side_btn(@click="now_device.count++") +
+              span.input_side_btn(@click="now_device_profile.count-=now_device_profile.count>0?1:0") -
+              input(type="number" ,v-model="now_device_profile.count")
+              span.input_side_btn(@click="now_device_profile.count++") +
             .form-group
               label 平均使用
               br
               .btn_group_inline(v-if="now_device")
-                button.btn(:class="{active:now_device.option==0}",@click="now_device.option=0")
+                button.btn(:class="{active:now_device_profile.option==0}",@click="now_device_profile.option=0")
                   span 很少
                   span.degree {{now_device.rarely}}hr
-                button.btn(:class="{active:now_device.option==1}",@click="now_device.option=1")
+                button.btn(:class="{active:now_device_profile.option==1}",@click="now_device_profile.option=1")
                   span 偶爾
                   span.degree {{now_device.occasionally}}hr
-                button.btn(:class="{active:now_device.option==2}",@click="now_device.option=2")
+                button.btn(:class="{active:now_device_profile.option==2}",@click="now_device_profile.option=2")
                   span 經常
                   span.degree {{now_device.often}}hr
-                button.btn(:class="{active:now_device.option==3}",@click="now_device.option=3")
+                button.btn(:class="{active:now_device_profile.option==3}",@click="now_device_profile.option=3")
                   span 頻繁
                   span.degree {{now_device.frequently}}hr
 
             .form-group
               label 購買年份
               br
-              .btn_group_inline(v-if="now_device")
+              .btn_group_inline(v-if="now_device_profile")
                 button.btn(v-for="op in get_year_options(now_device.year_options)" ,
-                           :class="{active:now_device.buy_time==op.value}",
-                           @click="now_device.buy_time=op.value")
+                           :class="{active:now_device_profile.buy_time==op.value}",
+                           @click="now_device_profile.buy_time=op.value")
                   span {{op.label}}
                   span.degree {{op.degree}}
                
@@ -130,6 +139,7 @@ export default {
       device_data.forEach(obj=>{obj.option=0})
       device_data.forEach(obj=>{obj.light_option=0})
       device_data.forEach(obj=>{obj.buy_time=""})
+      device_data.forEach(obj=>{obj.alter_specs=[]})
       device_data.forEach(obj=>{obj.consumption=obj.default_consumption})
       this.devices=device_data
       console.log(device_data)
@@ -145,9 +155,14 @@ export default {
       return this.devices
             .filter( device => (device.place==this.rooms[this.now_place_id].name) );
     },
+    //當前的電器
     now_device(){
       if (this.now_device_id==-1) return null;
       return this.filter_device[this.now_device_id];
+    },
+    //根據alt id 選擇主規格或是alter_specs裡面的自訂規格
+    now_device_profile(){   
+      return this.alter_id==-1 ? this.now_device : this.now_device.alter_specs[this.alter_id]
     },
     inPageRange(){
       let sc = this.scrollTop
@@ -169,52 +184,73 @@ export default {
       var room_sum=[0,0,0,0];
 
       //加總電器
-      this.devices.forEach((d,i)=>{
+      this.devices.forEach((device,i)=>{
         var cump=0;
-        //不同計算消耗電的方式
-        if (d.type=="normal"){
-          cump=d.consumption;
-        }
-        if (d.type=="light"){
-          cump=this.light_list[d.light_option].value;
-          
-        }if (d.type=="hotwater"){
-          cump=1070;
-        }
 
-        var hour=[d.rarely,d.occasionally,d.often,d.frequently][d.option];
-        var device_consumption= parseInt(cump*d.count*d.consumption_mul*d.day
-                *hour/1000 );
-
-        //老舊加乘
-        let optiontext = d.buy_time.replace("+","")
-        if (optiontext.indexOf("-")!=-1) optiontext=optiontext.split("-")[0]
-        let is_old = parseInt(optiontext)>=d.old_condition && d.old_condition!=-1
+        device.hour_consumption = 0
+        device.device_consumption = 0
         
-        if (is_old){
-          device_consumption*=1.5
-        }
+        //主規格與次要規格foreach
+        var profiles = 
+          [device].concat(device.alter_specs)
+                  .filter(o=>o.count>0)
 
-        if (d.type=="light"){
-          light_total +=device_consumption;
-        } 
-        total_c+=device_consumption;
+        
+        profiles.forEach(profile=>{
+          //不同計算消耗電的方式
+          if (device.type=="normal"){
+            cump=profile.consumption;
+          }
+          if (device.type=="light"){
+            cump=this.light_list[profile.light_option].value;
+            
+          }if (device.type=="hotwater"){
+            cump=1070;
+          }
 
-        if (d.count>0){
+          //計算使用時間 ＊ 單位時間能耗
+          var hour=[device.rarely,device.occasionally,device.often,device.frequently][profile.option];
+          var device_consumption= parseInt(cump*profile.count*device.consumption_mul*device.day
+                  *hour/1000 );
+
+          //老舊加乘
+          let optiontext = profile.buy_time.replace("+","")
+          if (optiontext.indexOf("-")!=-1) optiontext=optiontext.split("-")[0]
+          let is_old = parseInt(optiontext)>=device.old_condition && device.old_condition!=-1
+          
+          if (is_old){
+            device_consumption*=1.5
+          }
+
+          if (device.type=="light"){
+            light_total +=device_consumption;
+          } 
+          total_c+=device_consumption;
+
+        
           log_list.push({
-            content: d.count+" x "+d.name 
-                     + " ("+["很少","偶爾","經常","頻繁"][d.option]+")"
-                     +(is_old?"(老舊*1.5)":"") + " : "
-            //+" ("+d.place+"):  "
-            //+cump+"*"+d.consumption_mul+"*"+hour+"hr *"+d.day+" = "
+            content: profile.count+" x "+device.name 
+                    + " ("+["很少","偶爾","經常","頻繁"][profile.option]+")"
+                    +(is_old?"(老舊*1.5)":"") + " : "
+            //+" ("+device.place+"):  "
+            //+cump+"*"+device.consumption_mul+"*"+hour+"hr *"+device.day+" = "
             +device_consumption+" (度 / 年)",
-            place: d.place});
-        }
-        //設定設備清單上的單電器消耗量
-        d.hour_consumption = cump;
-        d.device_consumption = device_consumption;
-        room_sum[this.get_place_id(d.place)]+=device_consumption;
+            place: device.place});
+        
+          //設定設備清單上的單電器消耗量
+          device.hour_consumption += cump;
+          device.device_consumption += device_consumption;
+
+          //加總到房間總耗電量
+          room_sum[this.get_place_id(device.place)]+=device_consumption;
+        })
+            
+        //如果沒有任何成立的profile 預設值為0
+        device.hour_consumption = device.hour_consumption || 0
+        device.device_consumption = device.device_consumption || 0
+      
       });
+    
 
       //處理預設照明
       if (light_total==0){
@@ -248,6 +284,21 @@ export default {
     ...mapState(['house_area_size','site_width','scrollTop'])
   },
   methods: {
+    //加入一個自訂規格
+    add_other_spec(device){
+      if (!device.alter_specs){
+        device.alter_specs = []
+      }
+      device.alter_specs.push({
+        consumption: 0,
+        type: device.type,
+        count: 0,
+        buy_time: "",
+        option: 0,
+        light_option: 0
+      });
+      console.log(device.alter_specs)
+    },
     get_year_options(op_array_text){
       let ar = JSON.parse(op_array_text)
       let options = []
@@ -297,6 +348,7 @@ export default {
     },
     switch_device(id){
       this.now_device_id=id;
+      this.alter_id=-1;
       // this.now_device.count=1;
     },
     switch_place(id){
@@ -327,6 +379,7 @@ export default {
       now_device_id: 0,
       rooms,
       user_filled: false,
+      alter_id: -1,
       light_list: [
         {
           name: "省電燈泡",
